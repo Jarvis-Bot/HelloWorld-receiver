057a33759ea59ad9fd959052eb622902f28124e9
Victor Bersy <victorbersy+github@gmail.com>
2014-05-10 18:28:32 +0200

    update for Jarvis version 2.0.0 release

--- init.rb
+++ init.rb
@@ -1,13 +1,22 @@
 class HelloWorld
   def initialize(args)
     @tweet_object = args[:object]
-    @twitter_client = Jarvis::ThirdParty::Registered.client(:twitter)
-    greet if @tweet_object.is_a? Twitter::Tweet
+    @twitter_client = Jarvis::API::Addons.client(:twitter)
+    greet if match_hello? @tweet_object.text
   end
 
   def greet
     message = 'Hello sir, what can I do for you?'
-    options = { in_reply_to_status: @tweet_object  }
-    @twitter_client.update("@#{@tweet_object.user.screen_name} #{message}", options)
+    if @tweet_object.is_a? Twitter::Tweet
+      options = { in_reply_to_status: @tweet_object  }
+      @twitter_client.update("@#{@tweet_object.user.screen_name} #{message}", options)
+    elsif @tweet_object.is_a? Twitter::DirectMessage
+      user = @tweet_object.sender.id
+      @twitter_client.create_direct_message(user, message)
+    end
+  end
+
+  def match_hello?(text)
+    text.match(/\b(hello|hi|bonjour|hola|yo)\b/)
   end
 end
